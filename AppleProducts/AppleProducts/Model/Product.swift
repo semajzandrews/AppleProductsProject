5e479379b8fb2b1276a9f4e29e2c93757569fc8c
MCS <mcs@MCSs-MacBook-Pro.local>
2020-01-18 11:03:39 -0500

    Added Model Folder, created skeleton for Product and Product Rating

---
+++ AppleProducts/AppleProducts/Model/Product.swift
@@ -0,0 +1,27 @@
+//
+//  Product.swift
+//  AppleProducts
+//
+//  Created by MCS on 1/18/20.
+//  Copyright © 2020 MCS. All rights reserved.
+//
+
+import UIKit
+
+enum ProductRating {
+    
+    case unrated
+    case average
+    case ok
+    case good
+    case brilliant
+    
+}
+
+class Product {
+    
+    var image: UIImage
+    var title: String
+    var description: String
+    
+}
