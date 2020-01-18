a25d12173bd8cc53688474cc77915ac1f76082a3
MCS <mcs@MCSs-MacBook-Pro.local>
2020-01-18 11:34:54 -0500

    Adding Model for Product Line

--- AppleProducts/AppleProducts/Model/Product.swift
+++ AppleProducts/AppleProducts/Model/Product.swift
@@ -23,5 +23,18 @@ class Product {
     var image: UIImage
     var title: String
     var description: String
+    var rating: ProductRating
     
+    init(title: String, description: String, imageName: String){
+        self.title = title
+        self.description = description
+        
+        if let image = UIImage(named: imageName){
+            self.image = image
+        } else {
+            self.image = UIImage(named: "default")!
+        }
+        
+        rating = .unrated
+    }
 }
---
+++ AppleProducts/AppleProducts/Model/ProductLine.swift
@@ -0,0 +1,9 @@
+//
+//  ProductLine.swift
+//  AppleProducts
+//
+//  Created by MCS on 1/18/20.
+//  Copyright © 2020 MCS. All rights reserved.
+//
+
+import Foundation
